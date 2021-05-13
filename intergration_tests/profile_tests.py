85a604d9261397ee2222f6823740ab5310d11bfb
Nelson Liao <nliao@thoughtworks.com>
2021-05-12 21:05:15 -0700

    enhanced get_profile test, passing auth token and asserting on payload

--- intergration_tests/profile_tests.py
+++ intergration_tests/profile_tests.py
@@ -2,6 +2,13 @@ import requests
 
 
 def test_get_profile():
+    token = 'ca9573ebe00537727b4d9e882f9a8f27e96de949'
     url = 'http://localhost:8000/api/profile/'
-    resp = requests.get(url)
+    header = {
+        'Authorization': 'Token ' + token
+    }
+    resp = requests.get(url, headers=header)
     assert resp.status_code == 200
+    resp_body = resp.json()
+    assert resp_body[0]['email'] == 'admin@test.com'
+    assert resp_body[0]['name'] == 'Nelson'

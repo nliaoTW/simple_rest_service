f7c946bc4995a29d40260b4502d95ebd3818e921
Nelson Liao <nliao@thoughtworks.com>
2021-05-16 18:28:52 -0700

    removed un-needed libraries from views

--- profile_api/views/HelloApiView.py
+++ profile_api/views/HelloApiView.py
@@ -1,16 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework import filters
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticated
 
 from profile_api import serializers
-from profile_api import models
-from profile_api import permissions
 
 
 class HelloApiView(APIView):

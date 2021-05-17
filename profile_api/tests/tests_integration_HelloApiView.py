77265ee76988fb18c18b757e499df8ec78fc4d51
Nelson Liao <nliao@thoughtworks.com>
2021-05-16 21:40:48 -0700

    refactored out tests by type

--- profile_api/tests/tests_integration_HelloApiView.py
+++ profile_api/tests/tests_integration_HelloApiView.py
@@ -1,41 +1,7 @@
 from django.test import TestCase
-from rest_framework.test import APIRequestFactory, APIClient
-from profile_api.views.HelloApiView import HelloApiView
+from rest_framework.test import APIClient
 
-class HelloAPiViewUnitTests(TestCase):
-
-    def test_get(self):
-        req = APIRequestFactory().get('/')
-        resp = HelloApiView.get(self, req)
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data['message'], 'Hello!')
-        self.assertEqual(resp.data['an_apiview'], [
-            'Users HTTP method as function (get, post, patch, put, delete)',
-            'Is similar to a traditional Django View',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to URLs'
-        ])
-
-    def test_put(self):
-        req = APIRequestFactory().put('/')
-        resp = HelloApiView.put(self, req)
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data['method'], 'PUT')
-
-    def test_patch(self):
-        req = APIRequestFactory().patch('/')
-        resp = HelloApiView.patch(self, req)
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data['method'], 'PATCH')
-
-    def test_delete(self):
-        req = APIRequestFactory().delete('/')
-        resp = HelloApiView.delete(self, req)
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(resp.data['method'], 'DELETE')
-
-
-class HelloAPiViewIntegrationTests(TestCase):
+class HelloApiViewIntegrationTests(TestCase):
 
     def test_get(self):
         self.client = APIClient();
---
+++ profile_api/tests/tests_unit_HelloApiView.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from profile_api.views.HelloApiView import HelloApiView
+
+class HelloApiViewUnitTests(TestCase):
+
+    def test_get(self):
+        req = APIRequestFactory().get('/')
+        resp = HelloApiView.get(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['message'], 'Hello!')
+        self.assertEqual(resp.data['an_apiview'], [
+            'Users HTTP method as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs'
+        ])
+
+    def test_put(self):
+        req = APIRequestFactory().put('/')
+        resp = HelloApiView.put(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'PUT')
+
+    def test_patch(self):
+        req = APIRequestFactory().patch('/')
+        resp = HelloApiView.patch(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'PATCH')
+
+    def test_delete(self):
+        req = APIRequestFactory().delete('/')
+        resp = HelloApiView.delete(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'DELETE')
+

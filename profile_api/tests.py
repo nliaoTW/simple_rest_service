e1545f62174843a4ffbeedf617ed74503511adfa
Nelson Liao <nliao@thoughtworks.com>
2021-05-16 20:48:18 -0700

    added get/put/patch/delete unit tests for HelloApiView

--- profile_api/tests.py
+++ profile_api/tests.py
@@ -1,13 +1,35 @@
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 from profile_api.views.HelloApiView import HelloApiView
 
 class HelloAPiView(TestCase):
 
     def test_get(self):
-        status_code = 200
-        view_class = HelloApiView
+        req = APIRequestFactory().get('/')
+        resp = HelloApiView.get(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['message'], 'Hello!')
+        self.assertEqual(resp.data['an_apiview'], [
+            'Users HTTP method as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs'
+        ])
 
-        # req = RequestFactory().get('/')
-        # resp = view_class.get()(req, *[], **{})
-        # self.assertEqual(resp.status_code, 200)
+    def test_put(self):
+        req = APIRequestFactory().put('/')
+        resp = HelloApiView.put(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'PUT')
 
+    def test_patch(self):
+        req = APIRequestFactory().patch('/')
+        resp = HelloApiView.patch(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'PATCH')
+
+    def test_delete(self):
+        req = APIRequestFactory().delete('/')
+        resp = HelloApiView.delete(self, req)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data['method'], 'DELETE')
